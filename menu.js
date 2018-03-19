ed4c21ff57739e01fa7a13f9123c8172ef1d534d
Fábio Correia <fabioandre15@hotmail.com>
2018-03-19 09:10:51 +0000

    Updated Dateformat

--- menu.js
+++ menu.js
@@ -12,7 +12,7 @@ module.exports = {
             let week = getWeek(currentDate);
             getMenuByDateInterval(week.monday, week.friday).then(menu => {
                 const currentDayMenu = menu[currentDate.getDay() - 1];
-                const text = `Menu ${currentDate.toLocaleDateString('pt-PT')}:  
+                const text = `Menu ${currentDate.toISOString().split("T")[0]}:
                 Sopa: ${currentDayMenu.sopa}  
                 Carne: ${currentDayMenu.carne}  
                 Peixe: ${currentDayMenu.peixe}  
